c254fd82f6adc802b7ac9760d8090e412e6b8b98
vdefives <vincent.defives@enseirb-matmeca.fr>
2017-05-23 21:08:36 +0200

    dire_bonjour.html

---
+++ Xtension/function.js
@@ -0,0 +1,4 @@
+function saluer()
+{
+    alert("Ah !")
+}
---
+++ Xtension/script.js
@@ -0,0 +1,3 @@
+document.getElementById('b').addEventListener('click', function(){
+    document.getElementById('a').innerHTML='<strong>Bonjour</strong>';
+}, false);
