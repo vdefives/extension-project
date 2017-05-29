c161e523ac7a56e72efb711c83ac22d56d2e803e
vdefives <vincent.defives@enseirb-matmeca.fr>
2017-05-29 21:04:53 +0200

    Ajouts divers

---
+++ Xtension/lm.js
@@ -0,0 +1,16 @@
+var scripts = document.querySelectorAll("noscript");
+alert("COUCOU !");
+
+var un = 0;
+
+for (var i = 0 ; i < scripts.length ; i++) {
+    scripts[i].innerHTML = "Bonjour";
+    if ( un = 0)
+    {
+        alert("Et de un !");
+        un = 1;
+    }
+}
+
+
+
